e9d677148e9de69b93b242799643ee24fc0864fa
Alex Nichol <unixpickle@gmail.com>
2016-09-15 11:19:05 -0400

    added hebbnet to rnn tests
    The linear hebbnet does surprisingly well on XOR and Match Open. It also nears convergence on the matching/repeat tasks, but it never quite gets there (similar to other non-stack models).

---
+++ rnn/hebbnet.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/unixpickle/hebbnet"
+	"github.com/unixpickle/weakai/rnn"
+)
+
+// NewHebbNet creates a Hebbian network model.
+// The arguments are similar to NewLSTM.
+func NewHebbNet(inSize, hiddenSize, hiddenCount, outHiddenSize, outCount int) *Model {
+	return NewBlockModel(inSize, hiddenSize, hiddenCount, outHiddenSize,
+		outCount, func(i, h int) rnn.Block {
+			return hebbnet.NewDenseLayer(i, h)
+		})
+}
